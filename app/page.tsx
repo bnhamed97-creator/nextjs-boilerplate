04bf4dd11fe3149b91ae04b8055021c96240c4a6
bnhamed97-creator <bnhamed97@gmail.com>
2026-03-31 02:00:19 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,40 @@
-<div style={{ marginTop: "40px" }}>
-  <a href="https://wa.me/966554659799">
-    <button style={{
-      background: "green",
-      color: "white",
-      padding: "15px",
-      border: "none",
-      borderRadius: "10px",
-      width: "80%",
-      marginBottom: "10px"
-    }}>
-      تواصل واتساب
-    </button>
-  </a>
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      
+      <h1>مأذون شرعي بالمدينة المنورة</h1>
 
-  <br />
+      <p>توثيق عقود الزواج - سرعة وإنجاز</p>
 
-  <a href="tel:0554659799">
-    <button style={{
-      background: "red",
-      color: "white",
-      padding: "15px",
-      border: "none",
-      borderRadius: "10px",
-      width: "80%"
-    }}>
-      للاتصال
-    </button>
-  </a>
-</div>
+      <a href="https://wa.me/966554659799">
+        <button style={{
+          background: "green",
+          color: "white",
+          padding: "15px",
+          border: "none",
+          borderRadius: "10px",
+          width: "80%",
+          marginBottom: "10px"
+        }}>
+          تواصل واتساب
+        </button>
+      </a>
+
+      <br />
+
+      <a href="tel:0554659799">
+        <button style={{
+          background: "red",
+          color: "white",
+          padding: "15px",
+          border: "none",
+          borderRadius: "10px",
+          width: "80%"
+        }}>
+          اتصال
+        </button>
+      </a>
+
+    </div>
+  );
+}
