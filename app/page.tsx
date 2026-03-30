6e2c8f0bc78baef27246d8fd377d23cac9adc00e
bnhamed97-creator <bnhamed97@gmail.com>
2026-03-31 02:09:41 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,18 @@
 export default function Home() {
   return (
-    <div style={{ textAlign: "center", marginTop: "50px", fontFamily: "sans-serif" }}>
-      
+    <main style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>مأذون شرعي بالمدينة المنورة</h1>
-
       <p>توثيق عقود الزواج - سرعة وإنجاز</p>
 
       <a href="https://wa.me/966554659799">
-        <button style={{
-          background: "green",
-          color: "white",
-          padding: "15px",
-          border: "none",
-          borderRadius: "10px",
-          width: "80%",
-          marginBottom: "10px",
-          fontSize: "18px"
-        }}>
-          تواصل واتساب
-        </button>
+        <button>تواصل واتساب</button>
       </a>
 
-      <br />
+      <br /><br />
 
       <a href="tel:0554659799">
-        <button style={{
-          background: "red",
-          color: "white",
-          padding: "15px",
-          border: "none",
-          borderRadius: "10px",
-          width: "80%",
-          fontSize: "18px"
-        }}>
-          اتصال
-        </button>
+        <button>اتصال</button>
       </a>
-
-    </div>
+    </main>
   );
 }
