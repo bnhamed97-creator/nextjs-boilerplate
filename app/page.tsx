8ac1a06ce45250c7080da3e8b8ea53b92f80e2d2
bnhamed97-creator <bnhamed97@gmail.com>
2026-03-31 02:12:30 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,16 @@
-export default function Home() {
+export const metadata = {
+  title: "مأذون شرعي بالمدينة المنورة",
+  description: "توثيق عقود الزواج - سرعة وإنجاز",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <main style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>مأذون شرعي بالمدينة المنورة</h1>
-      <p>توثيق عقود الزواج - سرعة وإنجاز</p>
-
-      <a href="https://wa.me/966554659799">
-        <button>تواصل واتساب</button>
-      </a>
-
-      <br /><br />
-
-      <a href="tel:0554659799">
-        <button>اتصال</button>
-      </a>
-    </main>
+    <html lang="ar">
+      <body>{children}</body>
+    </html>
   );
 }
