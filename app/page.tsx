9786825621031419f9a7b9622ca75408ab278c10
bnhamed97-creator <bnhamed97@gmail.com>
2026-03-31 02:18:47 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,46 @@
-export const metadata = {
-  title: "مأذون شرعي بالمدينة المنورة",
-  description: "توثيق عقود الزواج - سرعة وإنجاز",
-};
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function Home() {
   return (
-    <html lang="ar">
-      <body>{children}</body>
-    </html>
+    <main style={{
+      textAlign: "center",
+      marginTop: "50px",
+      background: "#0b3d2e",
+      color: "white",
+      height: "100vh",
+      padding: "20px"
+    }}>
+      <h1>مأذون شرعي بالمدينة المنورة</h1>
+      <p>توثيق عقود الزواج - سرعة وإنجاز</p>
+
+      <br />
+
+      <a href="https://wa.me/966554659799">
+        <button style={{
+          background: "green",
+          color: "white",
+          padding: "15px",
+          border: "none",
+          borderRadius: "10px",
+          width: "80%",
+          marginBottom: "10px"
+        }}>
+          تواصل واتساب
+        </button>
+      </a>
+
+      <br />
+
+      <a href="tel:0554659799">
+        <button style={{
+          background: "red",
+          color: "white",
+          padding: "15px",
+          border: "none",
+          borderRadius: "10px",
+          width: "80%"
+        }}>
+          اتصال
+        </button>
+      </a>
+    </main>
   );
 }
