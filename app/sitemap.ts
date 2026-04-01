abd2df32ee8b6ecf09d76ba8de37daefb6ec8405
bnhamed97-creator <bnhamed97@gmail.com>
2026-04-01 10:31:56 +0300

    Create sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://mathoonshari.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://mathoonshari.com/services",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://mathoonshari.com/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://mathoonshari.com/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
