b05a3d0a060fbc2b3fc6d4fa237e64afb03eae0a
bnhamed97-creator <bnhamed97@gmail.com>
2026-04-01 06:53:15 +0300

    Create page.tsx

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>من نحن</h1>
+      <p>مأذون شرعي في المدينة المنورة لتوثيق عقود الزواج بسرعة وإنجاز.</p>
+    </div>
+  );
+}
